4969b121d5c35a39aa5442cf5ec7bb86931dfd45
Narumichan <withergames47@gmail.com>
2026-04-04 23:36:01 +0200

    added images

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 $().ready(function() {
-    console.log("js is ready");
+    console.log("doc is ready");
     $.getJSON("https://narumichan.github.io/energydrinks/tierlist.json", function(tierlistData){
         console.log(tierlistData);
+
+        const tierlistDiv = $("#compactTierlistDiv");
+        tierlistDiv.empty();
+
+        tierlistDiv.append($("<table></table>"));
+        const tierlistTable = $("#compactTierlistDiv>table");
+        
+        $.each(tierlistData["tiers"], function(i, tier){
+            tierlistTable.append($("<tr></tr>"));
+            let tierRow = tierlistTable.find($(`tr:nth-child(${i+1})`));
+            //let tierRow = $(`#compactTierlistDiv tr:nth-child(${i+1})`);
+
+            tierRow.append($("<td class='tier'></td>").text(`${tier["tierName"]}`));
+            
+            $.each(tier["drinks"], function(j, drink){
+                tierRow.append($("<td class='drinkCell'></td>"));
+                let drinkCell = tierRow.find($(`td:nth-child(${j+2})`));
+                drinkCell.append($(`<img src='${drink["image"]}'>`))
+            });
+        });
+
+
     });
  });

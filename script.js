13766e0f537026155240c3d90b3e1c6c46e9649a
Narumichan <withergames47@gmail.com>
2026-04-05 10:33:20 +0200

    images

--- script.js
+++ script.js
@@ -7,22 +7,32 @@ $().ready(function() {
         tierlistDiv.empty();
 
         tierlistDiv.append($("<table></table>"));
+        tierlistDiv.after($("<div id='drinkNotes'></div>"));
+        const drinkNotes = $("div#drinkNotes");
         const tierlistTable = $("#compactTierlistDiv>table");
         
+        let drinkIndex = 0;
         $.each(tierlistData["tiers"], function(i, tier){
             tierlistTable.append($("<tr></tr>"));
             let tierRow = tierlistTable.find($(`tr:nth-child(${i+1})`));
-            //let tierRow = $(`#compactTierlistDiv tr:nth-child(${i+1})`);
 
             tierRow.append($("<td class='tier'></td>").text(`${tier["tierName"]}`));
             
             $.each(tier["drinks"], function(j, drink){
+                drinkIndex++;
+                //console.log(drinkIndex)
+                //console.log(`${tier["tierName"]} : ${drink["name"]}`);
                 tierRow.append($("<td class='drinkCell'></td>"));
                 let drinkCell = tierRow.find($(`td:nth-child(${j+2})`));
-                drinkCell.append($(`<img src='${drink["image"]}'>`))
+                drinkCell.append($(`<img src='${drink["image"]}'>`));
+
+
+                drinkNotes.append($("<div class='drinkNote'></div>"));
+                let drinkNote = drinkNotes.find($(`.drinkNote:nth-child(${drinkIndex})`));
+                drinkNote.append($(`<img src='${drink["image"]}'>`));
+                drinkNote.append($('<p></p>').text(drink["notes"]));
             });
         });
-
 
     });
  });
